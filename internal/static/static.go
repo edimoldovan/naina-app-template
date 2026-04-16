5f8434500e0853639d434622d9781d31a3038dfd
ed <edimoldovan@gmail.com>
2026-04-16 11:10:03 +0200

    nocache

--- internal/static/static.go
+++ internal/static/static.go
@@ -14,9 +14,18 @@ var embeddedAssets embed.FS
 func Setup(r chi.Router) {
 	if config.IsDev() {
 		fs := http.FileServer(http.Dir("internal/static"))
-		r.Get("/assets/*", http.StripPrefix("/", fs).ServeHTTP)
+		r.Get("/assets/*", http.StripPrefix("/", noCache(fs)).ServeHTTP)
 	} else {
 		fs := http.FileServer(http.FS(embeddedAssets))
 		r.Get("/assets/*", http.StripPrefix("/", fs).ServeHTTP)
 	}
+}
+
+func noCache(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Cache-Control", "no-store, no-cache, must-revalidate, max-age=0")
+		w.Header().Set("Pragma", "no-cache")
+		w.Header().Set("Expires", "0")
+		h.ServeHTTP(w, r)
+	})
 }
